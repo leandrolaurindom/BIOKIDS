8222dacc5911daca400dc93fc60a19d29c5d37d0
Leandro <leandrolaurindom@gmail.com>
2026-03-09 13:28:06 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import path from 'path';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
     resolve: {
-      alias: { '@': path.resolve(__dirname, '.') },
-    },
-  };
+      alias: { '@': path.resolve(__dirname, '.') }
+    }
+  }
 })
